d959dabded79273313d2a5780bcd4e0d38930eb2
Khusniyarov-Arthur <Khusniyarovaa@yandex.ru>
2023-04-06 16:33:53 +0300

    Deploy end

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -13,6 +13,7 @@ export const Main = () => (
       <Tabs />
       <Routes>
         <Route exact path='/' element={<Home/>}/>
+        <Route exact path='/auth' element={<Home/>}/>
         <Route path='*' element={<Error/>}/>
         <Route path='/category/:page' element={<List />}>
           <Route path='post/:id' element={<Modal/>}/>
